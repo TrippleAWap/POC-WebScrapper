55d17c1e4325303570386e805e878354c79f6203
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2024-07-01 19:05:53 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const { JSDOM } = require('jsdom');
+const fs = require('node:fs');
+
+const page = "https://www.planetminecraft.com/forums/minecraft/servers/joinable-minecraft-realm-with-co-559977/";
+
+(async () => {
+    try {
+        var allPosts = JSON.parse(fs.readFileSync("./output.json", "utf8"));
+    } catch (err) {
+        var allPosts = {};
+    }
+    const dom = await JSDOM.fromURL(page);
+    const doc = dom.window.document;
+
+    const xpath = `/html/body/div[4]/div/div[2]/div[7]/*/div[1]/div[2]`
+    const evaluator = new dom.window.XPathEvaluator();
+    const result = evaluator.evaluate(xpath, doc, null, dom.window.XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    
+    for (let i = 0; i < result.snapshotLength; i++) {
+        const node = result.snapshotItem(i);
+        const parent = node.parentElement;
+        {
+            const username = parent.getElementsByClassName("member_info membertip ").item(0).getAttribute("href").split("/")[2]
+
+            const postTime = parent.getElementsByClassName("timeago").item(0).getAttribute("title");
+            let postMessage = parent.getElementsByClassName("core_read_more").item(0).innerHTML.replace(/<br>*<br>/g, "\n")
+
+            // parsing the post message to remove unnecessary tags and add links
+            postMessage = postMessage.replace(/<a href="([^"]+)"[^>]*>([^<]+)<\/a>/, (_, url, text) => {
+                url = url.split(" ")[0].trim();
+                text = text.trim();
+                if (url == text)
+                    return ` ${text}`;
+                return ` ${text} ( ${url} )`
+            });
+
+            // remove all html tags from the post message to make it plain text
+            postMessage = postMessage.replace(/<[^>]+>/g, "");
+
+            allPosts[parent.parentElement.getAttribute("data-id")] = {
+                username: username,
+                post: {
+                    uploadDate: Date(postTime),
+                    content: postMessage
+                }
+            };
+        }
+    }
+    fs.writeFileSync("./output.json", JSON.stringify(allPosts, null, 4))
+})();
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Planet Minecraft Scrapper
+
+This is a NodeJS script that scrapes the Planet Minecraft forums and extracts the latest posts from the Minecraft Realms category. It then saves the posts to a JSON file. This script was made as a proof of concept and is free to use under the MIT license.
+
+
+## Usage
+
+To use this script, you will need to have Node.js installed on your machine. Once you have Node.js installed, you can clone this repository and run the following command in your terminal:
+    `npm install`
+
+This will install the required dependencies for this script.
+
+Next, you can run the script by running the following command:
+    `node .`
+
+
+## License
+Copyright © 2024 TrippleAWap
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
